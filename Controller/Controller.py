3dc34decf95e971b0e9610428c62eaae59e32e50
javierocrono <juglerhak@hotmail.com>
2014-07-14 22:50:13 -0400

    creando controller

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -1,1 +1,29 @@
+from model import Paciente, Medico
+
+
+def pacientes():
+    return Paciente.all()
+
+
+def medicos():
+    return Medico.all()
+
+
+def crear_paciente(rut, nombres, apellidos, ficha_medica):
+    """
+    Método que crea un Paciente. Lo correcto sería validar
+    que toda la información es correcta
+    Ej:
+    """
+    nuevo = Paciente()
+    nuevo.rut = rut
+    # Aquí podrían haber validaciones para el codigo
+    nuevo.nombres = nombres
+    nuevo.apellidos = apellidos
+    nuevo.ficha_medica = ficha_medica
+    nuevo.save()
+
+if __name__ == "__main__":
+    pass
+
 # -*- coding: utf-8 -*-
